3ad9c9600c10256f347a07c3e998f37db05c0b22
Frederick Solt <frederick-solt@uiowa.edu>
2020-10-19 12:40:38 -0500

    update for v9.0

--- server.R
+++ server.R
@@ -125,7 +125,7 @@ shinyServer(function(input, output, session) {
             s1$series <- paste(s1$country, s1$variable, sep=", ")
         }
 
-        note1 <- "Note: Solid lines indicate mean estimates; shaded regions indicate the associated 95% uncertainty intervals.\nSource: Standardized World Income Inequality Database v8.3 (Solt 2020)."
+        note1 <- "Note: Solid lines indicate mean estimates; shaded regions indicate the associated 95% uncertainty intervals.\nSource: Standardized World Income Inequality Database v9.0 (Solt 2020)."
                 
         # Basic plot
         p <- ggplot(s1, aes(x=year, y=value, colour=series)) + 
